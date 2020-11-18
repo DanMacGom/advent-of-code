3d3c2d52e0c43b43a29ac0c56b8d97a7f68b0d97
Daniel Macías Gómez <daniel.macias.gomez@gmail.com>
2020-11-18 12:26:26 +0100

    Added Day1 part 2, Day6 part 2

--- 2015/Day1/Day1.py
+++ 2015/Day1/Day1.py
@@ -10,4 +10,21 @@ for symbol in puzzle:
     else:
         closed += 1
 
-print("Result:", opens - closed)
+print("Result part 1:", opens - closed)
+
+opens = 0
+closed = 0
+
+count = 1
+for symbol in puzzle:
+    if symbol == "(":
+        opens += 1
+    else:
+        closed += 1
+
+    if opens - closed < 0:
+        print("Result part 2:", count)
+        break
+
+    count += 1
+
--- 2015/Day6/Day6.py
+++ 2015/Day6/Day6.py
@@ -33,4 +33,36 @@ lights = 0
 for j in light_grid:
     lights += sum(a for a in j if a > 0)
 
-print(lights)
+print("Result part 1:", lights)
+
+# Part 2
+light_grid = [[0 for i in range(1000)] for j in range(1000)]
+
+for instruction in puzzle:
+    x1 = int(re.findall("\d+", instruction)[0])
+    y1 = int(re.findall("\d+", instruction)[1])
+
+    x2 = int(re.findall("\d+", instruction)[2])
+    y2 = int(re.findall("\d+", instruction)[3])
+
+    if "turn on" in instruction:
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                light_grid[i][j] += 1
+
+    if "turn off" in instruction:
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                light_grid[i][j] -= 1 if light_grid[i][j] > 0 else 0
+
+    if "toggle" in instruction:
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                light_grid[i][j] += 2
+
+lights = 0
+
+for j in light_grid:
+    lights += sum(a for a in j)
+
+print("Result part 2:", lights)
