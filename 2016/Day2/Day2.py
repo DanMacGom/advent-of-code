7747ef1751c4c7ea840844786856347d878bde4d
Daniel Macías Gómez <daniel.macias.gomez@gmail.com>
2020-12-09 20:15:00 +0100

    Added Day2

---
+++ 2016/Day2/Day2.py
@@ -0,0 +1,43 @@
+with open("d2_data") as f:
+    puzzle = f.read().split("\n")
+
+
+class Password:
+    def __init__(self, pad):
+        if pad == "numpad":
+            self.i, self.j = 2, 2
+            self.numpad = [[".", ".", ".", ".", "."],
+                           [".", "1", "2", "3", "."],
+                           [".", "4", "5", "6", "."],
+                           [".", "7", "8", "9", "."],
+                           [".", ".", ".", ".", "."]]
+        elif pad == "rhombus":
+            self.i, self.j = 3, 1
+            self.numpad = [[".", ".", ".", ".", ".", ".", "."],
+                           [".", ".", ".", "1", ".", ".", "."],
+                           [".", ".", "2", "3", "4", ".", "."],
+                           [".", "5", "6", "7", "8", "9", "."],
+                           [".", ".", "A", "B", "C", ".", "."],
+                           [".", ".", ".", "D", ".", ".", "."],
+                           [".", ".", ".", ".", ".", ".", "."]]
+
+        self.password = ""
+
+    def decrypt_numpad(self, inp):
+        for command in inp:
+            for ins in command:
+                if ins == "U" and self.numpad[self.i - 1][self.j] != ".":
+                    self.i -= 1
+                elif ins == "R" and self.numpad[self.i][self.j + 1] != ".":
+                    self.j += 1
+                elif ins == "D" and self.numpad[self.i + 1][self.j] != ".":
+                    self.i += 1
+                elif ins == "L" and self.numpad[self.i][self.j - 1] != ".":
+                    self.j -= 1
+
+            self.password += self.numpad[self.i][self.j]
+        return self.password
+
+
+password = Password("rhombus")
+print(password.decrypt_numpad(puzzle))
